21db0cd9b8c42f5c4a346ac0f5e322ba2ccda74a
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-13 23:08:52 +0900

    이동함수를 만들었다
    전달된 인수에 따라 방향을 다르게 해서
    직진하는 이동함수를 만들었다!
    이제 리셋함수랑
    키보드입력만 받아오면된다

---
+++ wasd.py
@@ -0,0 +1,13 @@
+import turtle
+
+def move(a):
+    if a == 0:
+        turtle.setheading(0)
+    elif a == 1:
+        turtle.setheading(90)
+    elif a == 2:
+        turtle.setheading(180)
+    else :
+        turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
